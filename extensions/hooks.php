9f1395c37d69315a3520b213fc7d527124ddfbe2
Johann Schopplich <mail@johannschopplich.com>
2023-11-16 14:32:00 +0100

    fix: page indexing after delete hook

--- extensions/hooks.php
+++ extensions/hooks.php
@@ -18,7 +18,22 @@ return [
         $index = $algolia->getAlgoliaIndex();
         $index->deleteObject($page->uri($languageCode));
     },
-    'page.*:after' => function (\Kirby\Cms\Event $event, \Kirby\Cms\Page $page) {
+    'page.delete:after' => function (bool $status, \Kirby\Cms\Page $page) {
+        /** @var \Kirby\Cms\App $kirby */
+        $kirby = $this;
+
+        if (
+            $kirby->option('johannschopplich.algolia-docsearch.hooks', false) !== true ||
+            $kirby->option('debug')
+        ) {
+            return;
+        }
+
+        $algolia = DocSearch::instance();
+        $index = $algolia->getAlgoliaIndex();
+        $index->deleteObject($page->uri($kirby->languageCode()));
+    },
+    'page.*:after' => function (\Kirby\Cms\Event $event, \Kirby\Cms\Page $newPage) {
         /** @var \Kirby\Cms\App $kirby */
         $kirby = $this;
 
@@ -30,24 +45,24 @@ return [
         }
 
         // Check if we want to handle the action
-        if (!in_array($event->action(), ['changeSlug', 'changeStatus', 'changeTitle', 'delete', 'update'], true)) {
+        if (!in_array($event->action(), ['changeSlug', 'changeStatus', 'changeTitle', 'update'], true)) {
             return;
         }
 
-        $languageCode = $page->kirby()->languageCode();
+        $languageCode = $kirby->languageCode();
         $algolia = DocSearch::instance();
         $index = $algolia->getAlgoliaIndex();
 
         if (
             $event->action() === 'delete' ||
-            ($event->action() === 'changeStatus' && !$page->isListed())
+            ($event->action() === 'changeStatus' && !$newPage->isListed())
         ) {
-            $index->deleteObject($page->uri($languageCode));
+            $index->deleteObject($newPage->uri($languageCode));
             return;
         }
 
         try {
-            $algolia->indexPage($page, $languageCode);
+            $algolia->indexPage($newPage, $languageCode);
         } catch (\Exception $e) {
             throw new \Exception($e->getMessage());
         }
