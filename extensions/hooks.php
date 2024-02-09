83c010d43cae3ea2f9f21c1f8c32fe7196dbcf35
Johann Schopplich <mail@johannschopplich.com>
2024-02-09 19:58:27 +0100

    fix: `Exception` usage inside namespace

--- extensions/hooks.php
+++ extensions/hooks.php
@@ -1,9 +1,9 @@
 <?php
 
 use JohannSchopplich\Algolia\DocSearch;
+use Kirby\Cms\Event;
 use Kirby\Cms\Page;
 use Kirby\Exception\Exception;
-use Throwable;
 
 return [
     'page.changeSlug:before' => function (Page $page, string $slug, string|null $languageCode = null) {
@@ -32,7 +32,7 @@ return [
         $index = $docSearch->getAlgoliaIndex();
         $index->deleteObject($page->uri($this->languageCode()));
     },
-    'page.*:after' => function (\Kirby\Cms\Event $event, Page|null $newPage) {
+    'page.*:after' => function (Event $event, Page|null $newPage) {
         /** @var \Kirby\Cms\App $this */
         if (
             $this->option('johannschopplich.algolia-docsearch.hooks', false) !== true ||
@@ -64,7 +64,7 @@ return [
 
         try {
             $docSearch->indexPage($newPage, $languageCode);
-        } catch (Throwable $e) {
+        } catch (\Exception $e) {
             throw new Exception('Algolia: ' . $e->getMessage());
         }
     }
