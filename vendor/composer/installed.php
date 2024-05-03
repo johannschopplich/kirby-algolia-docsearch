50504b9258f6a8b316a234642247e4d4c7b6b46c
Johann Schopplich <mail@johannschopplich.com>
2024-05-03 07:50:17 +0200

    chore: release v0.6.1

--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -1,8 +1,8 @@
 <?php return array(
     'root' => array(
         'name' => 'johannschopplich/kirby-algolia-docsearch',
-        'pretty_version' => '0.6.0',
-        'version' => '0.6.0.0',
+        'pretty_version' => '0.6.1',
+        'version' => '0.6.1.0',
         'reference' => null,
         'type' => 'kirby-plugin',
         'install_path' => __DIR__ . '/../../',
@@ -29,8 +29,8 @@
             'dev_requirement' => false,
         ),
         'johannschopplich/kirby-algolia-docsearch' => array(
-            'pretty_version' => '0.6.0',
-            'version' => '0.6.0.0',
+            'pretty_version' => '0.6.1',
+            'version' => '0.6.1.0',
             'reference' => null,
             'type' => 'kirby-plugin',
             'install_path' => __DIR__ . '/../../',
